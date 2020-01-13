d5d564e351890ddb23a35ad8c915d681d5dd1d1a
Dmitry Koshevoy <dk@nimses.com>
2020-01-13 20:56:32 +0200

    13 Jan 2020

--- CodeWars/Unfinished/HumanReadableTime.cs
+++ CodeWars/Unfinished/HumanReadableTime.cs
@@ -12,13 +12,17 @@ namespace CodeWars
 		/// Kata #52685f7382004e774f0001f7 <a href="https://www.codewars.com/kata/52685f7382004e774f0001f7">Human Readable Time</a>
 		/// </summary>
 		[Theory]
-		[InlineData(369999, "99:59:59")]
+		[InlineData(0, "00:00:00")]
+		[InlineData(5, "00:00:05")]
+		[InlineData(60, "00:01:00")]
+		[InlineData(86399, "23:59:59")]
+		[InlineData(359999, "99:59:59")]
 		public void HumanReadableTime(int seconds, string expected)
 		{
-			TimeSpan ts = TimeSpan.FromSeconds(seconds);
 			int hours = seconds / 3600;
-			int minutes = (seconds - seconds % 3600) / 60;
-			string result = "";
+			int minutes = seconds % 3600 / 60;
+			int secs = seconds % 60;
+			string result = $"{hours.ToString("00")}:{minutes.ToString("00")}:{secs.ToString("00")}";
 			Assert.Equal(expected, result);
 		}
 
