7c13a36bc4cfe3b373162353556ec25ddf9c7524
Dmitry Koshevoy <dk@nimses.com>
2020-01-14 15:46:46 +0200

    13 Jan 2020

---
+++ CodeWars/2020/Jan/MultiplesOf3Or5.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace CodeWars
+{
+
+	public partial class Kata_2020_Jan
+	{
+
+		/// <summary>
+		/// Kata #514b92a657cdc65150000006 <a href="https://www.codewars.com/kata/514b92a657cdc65150000006">Multiples of 3 or 5</a>
+		/// </summary>
+		[Theory]
+		[InlineData(10, 23)]
+		public void MultiplesOf3Or5(int value, int expected)
+		{
+			int result = value == 0 ? 0 : Enumerable.Range(1, value - 1).Where(c => c % 3 == 0 || c % 5 == 0).Sum();
+			Assert.Equal(expected, result);
+		}
+
+	}
+
+}
---
+++ CodeWars/2020/Jan/ShortestWord.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace CodeWars
+{
+
+	public partial class Kata_2020_Jan
+	{
+
+		/// <summary>
+		/// Kata #57cebe1dc6fdc20c57000ac9 <a href="https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9">Shortest word</a>
+		/// </summary>
+		[Theory]
+		[InlineData("a bc def", 1)]
+		[InlineData("bitcoin take over the world maybe who knows perhaps", 3)]
+		[InlineData("turns out random test cases are easier than writing out basic ones", 3)]
+		public void ShortestWord(string value, int expected)
+		{
+			int result = value.Split(' ').OrderBy(c => c.Length).First().Length;
+			Assert.Equal(expected, result);
+		}
+
+	}
+
+}
