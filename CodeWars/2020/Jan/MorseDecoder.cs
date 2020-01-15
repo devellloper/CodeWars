1a0f477afa2f42b7d03cb02bb5bad840b1ad22c7
Dmitry Koshevoy <dk@nimses.com>
2020-01-15 16:09:52 +0200

    15 Jan 2020

---
+++ CodeWars/2020/Jan/MorseDecoder.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Xunit;
+
+namespace CodeWars
+{
+
+	public partial class Kata_2020_Jan
+	{
+
+		/// <summary>
+		/// Kata #54b724efac3d5402db00065e <a href="https://www.codewars.com/kata/54b724efac3d5402db00065e">Decode the Morse code </a>
+		/// </summary>
+		[Theory]
+		[InlineData(".... . -.--   .--- ..- -.. .", "HEY JUDE")]
+		public void MorseDecoder(string morseCode, string expected)
+		{
+			StringBuilder sb = new StringBuilder();
+			morseCode.Split(new string[] { "   " }, System.StringSplitOptions.RemoveEmptyEntries);
+			string[] GetWords() => morseCode.Split(new string[] { "   " }, System.StringSplitOptions.RemoveEmptyEntries);
+			string[] GetChars(string code) => morseCode.Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+			foreach (string word in GetWords())
+			{
+				if (sb.Length > 0)
+					sb.Append(' ');
+				//foreach(string c in GetChars(word))
+					//sb.Append(MorseCode.Get(c)); //STATIC CLASS FROM KATA EDITOR
+			}
+			string result = sb.ToString();
+			Assert.Equal(expected, result);
+		}
+
+	}
+
+}
